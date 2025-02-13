8905f2ed4cea692fd23d8ab206635678065cfa83
0xthrpw <0xthrpw@gmail.com>
2025-02-12 23:48:21 -0500

    chore: forge clone 0x0E688f5DCa4a0a4729946ACbC44C792341714e08

---
+++ EFPList/src/interfaces/IEFPListRegistry.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+/**
+ * @title EFPListRegistry
+ * @notice A registry connecting token IDs with data such as managers, users, and list locations.
+ */
+interface IEFPListRegistry {
+  ///////////////////////////////////////////////////////////////////////////
+  // Enums
+  ///////////////////////////////////////////////////////////////////////////
+
+  enum MintState {
+    Disabled,
+    OwnerOnly,
+    PublicMint,
+    PublicBatch
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Events
+  ///////////////////////////////////////////////////////////////////////////
+
+  /// @notice Emitted when a list storage location is set
+  event UpdateListStorageLocation(uint256 indexed tokenId, bytes listStorageLocation);
+
+  ///////////////////////////////////////////////////////////////////////////
+  // ListStorageLocation
+  ///////////////////////////////////////////////////////////////////////////
+
+  function getListStorageLocation(uint256 tokenId) external view returns (bytes memory);
+
+  function setListStorageLocation(uint256 tokenId, bytes calldata listStorageLocation) external;
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Mint
+  ///////////////////////////////////////////////////////////////////////////
+
+  /// @notice Fetches the mint state.
+  function getMintState() external view returns (MintState);
+
+  /// @notice Sets the mint state.
+  /// @param _mintState The new mint state.
+  function setMintState(MintState _mintState) external;
+
+  /// @notice Fetches the max mint batch size.
+  function getMaxMintBatchSize() external view returns (uint256);
+
+  /// @notice Sets the max mint batch size.
+  /// @param _maxMintBatchSize The new max mint batch size.
+  function setMaxMintBatchSize(uint256 _maxMintBatchSize) external;
+
+  /// @notice Mints a new token.
+  function mint(bytes calldata listStorageLocation) external payable;
+
+  /**
+   * @notice Mints a new token to the given address.
+   * @param recipient The address to mint the token to.
+   */
+  function mintTo(address recipient, bytes calldata listStorageLocation) external payable;
+
+  /// @notice Mints a new token to the given address.
+  function mintBatch(uint256 quantity) external payable;
+
+  /// @notice Mints a new token to the given address.
+  function mintBatchTo(address recipient, uint256 quantity) external payable;
+}
