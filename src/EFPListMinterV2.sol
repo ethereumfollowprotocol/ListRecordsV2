b582ae488c25c7e4547ba9c52f40103ff951b2dc
0xthrpw <0xthrpw@gmail.com>
2025-02-13 20:45:58 -0500

    base contracts

---
+++ src/EFPListMinterV2.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {Ownable} from 'lib/openzeppelin-contracts/contracts/access/Ownable.sol';
+import {Pausable} from 'lib/openzeppelin-contracts/contracts/utils/Pausable.sol';
+import {IEFPAccountMetadata} from './interfaces/IEFPAccountMetadata.sol';
+import {IEFPListRegistry} from './interfaces/IEFPListRegistry.sol';
+import {IEFPListRecords} from './interfaces/IEFPListRecords.sol';
+import {ENSReverseClaimer} from './lib/ENSReverseClaimer.sol';
+
+interface IEFPListRegistryERC721 is IEFPListRegistry {
+  function ownerOf(uint256 tokenId) external view returns (address);
+
+  function totalSupply() external view returns (uint256);
+}
+
+/**
+ * @title EFPListMetadata
+
+ * @author throw; (0xthrpw.eth)
+ * @custom:contributor Cory Gabrielsen (cory.eth)
+ * @custom:benediction DEVS BENEDICAT ET PROTEGAT CONTRACTVS MEAM
+ *
+ * @notice This contract mints and assigns primary lists to users, and sets
+ * EFP List metadata.
+ */
+contract EFPListMinterV2 is ENSReverseClaimer, Pausable {
+  IEFPListRegistryERC721 public registry;
+  IEFPAccountMetadata public accountMetadata;
+  IEFPListRecords public listRecordsL1;
+
+  constructor(address _registryAddress, address _accountMetadataAddress, address _listRecordsL1) {
+    registry = IEFPListRegistryERC721(_registryAddress);
+    accountMetadata = IEFPAccountMetadata(_accountMetadataAddress);
+    listRecordsL1 = IEFPListRecords(_listRecordsL1);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Pausable
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @dev Pauses the contract. Can only be called by the contract owner.
+   */
+  function pause() public onlyOwner {
+    _pause();
+  }
+
+  /**
+   * @dev Unpauses the contract. Can only be called by the contract owner.
+   */
+  function unpause() public onlyOwner {
+    _unpause();
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // minting
+  /////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @dev Decode a list storage location with no metadata.
+   * @param listStorageLocation The storage location of the list.
+   * @return slot The slot of the list.
+   * @return contractAddress The contract address of the list.
+   */
+  function decodeL1ListStorageLocation(bytes calldata listStorageLocation) internal pure returns (uint256, address) {
+    // the list storage location is
+    // - version (1 byte)
+    // - list storate location type (1 byte)
+    // - chain id (32 bytes)
+    // - contract address (20 bytes)
+    // - slot (32 bytes)
+    require(listStorageLocation.length == 1 + 1 + 32 + 20 + 32, 'EFPListMinter: invalid list storage location');
+    require(listStorageLocation[0] == 0x01, 'EFPListMinter: invalid list storage location version');
+    require(listStorageLocation[1] == 0x01, 'EFPListMinter: invalid list storage location type');
+    address contractAddress = _bytesToAddress(listStorageLocation, 34);
+
+    uint256 slot = _bytesToUint(listStorageLocation, 54);
+    return (slot, contractAddress);
+  }
+
+  /**
+   * @dev Mint a primary list.
+   * @param listStorageLocation The storage location of the list.
+   */
+  function easyMint(bytes calldata listStorageLocation) public payable whenNotPaused {
+    // validate the list storage location
+    (uint256 slot, address recordsContract) = decodeL1ListStorageLocation(listStorageLocation);
+
+    uint256 tokenId = registry.totalSupply();
+    registry.mintTo{value: msg.value}(msg.sender, listStorageLocation);
+    _setDefaultListForAccount(msg.sender, tokenId);
+    if (recordsContract == address(listRecordsL1)) {
+      listRecordsL1.setListUser(slot, msg.sender);
+      listRecordsL1.setListManager(slot, msg.sender);
+    }
+  }
+
+  /**
+   * @dev Mint a primary list to a specific address.
+   * @param to The address to mint the list to.
+   * @param listStorageLocation The storage location of the list.
+   */
+  function easyMintTo(address to, bytes calldata listStorageLocation) public payable whenNotPaused {
+    // validate the list storage location
+    (uint256 slot, address recordsContract) = decodeL1ListStorageLocation(listStorageLocation);
+
+    uint256 tokenId = registry.totalSupply();
+    registry.mintTo{value: msg.value}(to, listStorageLocation);
+    _setDefaultListForAccount(msg.sender, tokenId);
+    if (recordsContract == address(listRecordsL1)) {
+      listRecordsL1.setListUser(slot, msg.sender);
+      listRecordsL1.setListManager(slot, msg.sender);
+    }
+  }
+
+  /**
+   * @dev Mint a primary list without metadata.
+   * @param listStorageLocation The storage location of the list.
+   */
+  function mintPrimaryListNoMeta(bytes calldata listStorageLocation) public payable whenNotPaused {
+    // validate the list storage location
+    decodeL1ListStorageLocation(listStorageLocation);
+    uint256 tokenId = registry.totalSupply();
+    _setDefaultListForAccount(msg.sender, tokenId);
+    registry.mintTo{value: msg.value}(msg.sender, listStorageLocation);
+  }
+
+  /**
+   * @dev Mint a primary list without metadata to a specific address.
+   * @param listStorageLocation The storage location of the list.
+   */
+  function mintNoMeta(bytes calldata listStorageLocation) public payable whenNotPaused {
+    // validate the list storage location
+    decodeL1ListStorageLocation(listStorageLocation);
+
+    registry.mintTo{value: msg.value}(msg.sender, listStorageLocation);
+  }
+
+  /**
+   * @dev Mint a primary list without metadata to a specific address.
+   * @param to The address to mint the list to.
+   * @param listStorageLocation The storage location of the list.
+   */
+  function mintToNoMeta(address to, bytes calldata listStorageLocation) public payable whenNotPaused {
+    // validate the list storage location
+    decodeL1ListStorageLocation(listStorageLocation);
+
+    registry.mintTo{value: msg.value}(to, listStorageLocation);
+  }
+
+  /**
+   * @dev Set the default list for an account.
+   * @param to The address to set the default list for.
+   * @param tokenId The token ID of the list.
+   */
+  function _setDefaultListForAccount(address to, uint256 tokenId) internal {
+    accountMetadata.setValueForAddress(to, 'primary-list', abi.encodePacked(tokenId));
+  }
+
+  function _getChainId() internal view returns (uint256) {
+    uint256 id;
+    assembly {
+      id := chainid()
+    }
+    return id;
+  }
+
+  // Generalized function to convert bytes to uint256 with a given offset
+  function _bytesToUint(bytes memory data, uint256 offset) internal pure returns (uint256) {
+    require(data.length >= offset + 32, 'Data too short');
+    uint256 value;
+    assembly {
+      value := mload(add(data, add(32, offset)))
+    }
+    return value;
+  }
+
+  // Helper function to convert bytes to address with a given offset
+  function _bytesToAddress(bytes memory data, uint256 offset) internal pure returns (address addr) {
+    require(data.length >= offset + 20, 'Data too short');
+    assembly {
+      // Extract 20 bytes from the specified offset
+      addr := mload(add(add(data, 20), offset))
+      // clear the 12 least significant bits of the address
+      addr := and(addr, 0x000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+    }
+    return addr;
+  }
+}
---
+++ test/EFPListRecords.t.sol
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import 'forge-std/Test.sol';
+import {EFPListRecordsV2} from '../src/EFPListRecordsV2.sol';
+
+contract EFPListRecordsTest is Test {
+  EFPListRecordsV2 public listRecords;
+  uint8 constant LIST_OP_VERSION = 1;
+  uint8 constant LIST_OP_TYPE_ADD_RECORD = 1;
+  uint8 constant LIST_OP_TYPE_REMOVE_RECORD = 2;
+  uint8 constant LIST_OP_TYPE_ADD_TAG = 3;
+  uint8 constant LIST_OP_TYPE_REMOVE_TAG = 4;
+  uint8 constant LIST_RECORD_VERSION = 1;
+  uint8 constant LIST_RECORD_TYPE_RAW_ADDRESS = 1;
+  address ADDRESS_1 = 0x0000000000000000000000000000000000AbC123;
+  address ADDRESS_2 = 0x0000000000000000000000000000000000DeF456;
+  address ADDRESS_3 = 0x0000000000000000000000000000000000789AbC;
+  uint256 constant NONCE = 0;
+  bytes4 constant Error_EnforcedPause = bytes4(keccak256('EnforcedPause()'));
+  bytes4 constant Error_NotListManagerSelector = bytes4(keccak256('NotListManager(address)'));
+  bytes constant Error_NotListManager = abi.encodeWithSelector(Error_NotListManagerSelector, address(1));
+
+  function setUp() public {
+    listRecords = new EFPListRecordsV2();
+  }
+
+  // Helper function to compare bytes
+  function _assertBytesEqual(bytes memory a, bytes memory b) internal pure {
+    assert(a.length == b.length);
+    for (uint256 i = 0; i < a.length; i++) {
+      assert(a[i] == b[i]);
+    }
+  }
+
+  function _encodeListOp(uint8 opType) internal view returns (bytes memory) {
+    bytes memory result = abi.encodePacked(
+      LIST_OP_VERSION, // Version for ListOp
+      opType, // Operation type for ListOp (Add Record)
+      LIST_RECORD_VERSION, // Version for ListRecord
+      LIST_RECORD_TYPE_RAW_ADDRESS, // Record type for ListRecord (Raw Address)
+      ADDRESS_1 // Raw address (20 bytes)
+    );
+    if (opType == LIST_OP_TYPE_ADD_TAG || opType == LIST_OP_TYPE_REMOVE_TAG) {
+      result = abi.encodePacked(result, 'tag');
+    }
+    return result;
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // pause
+  /////////////////////////////////////////////////////////////////////////////
+
+  function test_CanPause() public {
+    assertEq(listRecords.paused(), false);
+    listRecords.pause();
+    assertEq(listRecords.paused(), true);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // unpause
+  /////////////////////////////////////////////////////////////////////////////
+
+  function test_CanUnpause() public {
+    listRecords.pause();
+    listRecords.unpause();
+    assertEq(listRecords.paused(), false);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // claimListManager
+  /////////////////////////////////////////////////////////////////////////////
+
+  function test_CanClaimListManager() public {
+    listRecords.claimListManager(NONCE);
+    assertEq(listRecords.getListManager(NONCE), address(this));
+  }
+
+  function test_RevertIf_ClaimListManagerWhenPaused() public {
+    listRecords.pause();
+    vm.expectRevert(Error_EnforcedPause);
+    listRecords.claimListManager(NONCE);
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // setListManager
+  /////////////////////////////////////////////////////////////////////////////
+
+  function test_CanSetListManager() public {
+    listRecords.claimListManager(NONCE);
+    listRecords.setListManager(NONCE, address(1));
+    assertEq(listRecords.getListManager(NONCE), address(1));
+  }
+
+  function test_RevertIf_SetListManagerWhenPaused() public {
+    listRecords.claimListManager(NONCE);
+    listRecords.pause();
+    vm.expectRevert(Error_EnforcedPause);
+    listRecords.setListManager(NONCE, address(1));
+  }
+
+  function test_RevertIf_SetListManagerFromNonManager() public {
+    listRecords.claimListManager(NONCE);
+    vm.prank(address(1));
+    vm.expectRevert(Error_NotListManager);
+    listRecords.setListManager(NONCE, address(1));
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // applyListOp
+  /////////////////////////////////////////////////////////////////////////////
+
+  function _CanApplyListOp(uint8 opType) internal {
+    assertEq(listRecords.getListOpCount(NONCE), 0);
+
+    // listRecords.claimListManager(NONCE);
+
+    bytes memory listOp = _encodeListOp(opType);
+    listRecords.applyListOp(NONCE, listOp);
+
+    assertEq(listRecords.getListOpCount(NONCE), 1);
+    _assertBytesEqual(listRecords.getListOp(NONCE, 0), listOp);
+  }
+
+  function test_CanApplyListOpToAddRecord() public {
+    _CanApplyListOp(LIST_OP_TYPE_ADD_RECORD);
+  }
+
+  function test_CanApplyListOpToRemoveRecord() public {
+    _CanApplyListOp(LIST_OP_TYPE_REMOVE_RECORD);
+  }
+
+  function test_CanApplyListOpToAddTag() public {
+    _CanApplyListOp(LIST_OP_TYPE_ADD_TAG);
+  }
+
+  function test_CanApplyListOpToRemoveTag() public {
+    _CanApplyListOp(LIST_OP_TYPE_REMOVE_TAG);
+  }
+
+  function test_RevertIf_ApplyListOpWhenPaused() public {
+    listRecords.pause();
+    vm.expectRevert(Error_EnforcedPause);
+    listRecords.applyListOp(NONCE, _encodeListOp(LIST_OP_TYPE_ADD_RECORD));
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // applyListOps
+  /////////////////////////////////////////////////////////////////////////////
+
+  function test_CanApplyListOpsSingular() public {
+    assertEq(listRecords.getListOpCount(NONCE), 0);
+
+    bytes[] memory listOps = new bytes[](1);
+    listOps[0] = _encodeListOp(LIST_OP_TYPE_ADD_RECORD);
+    listRecords.applyListOps(NONCE, listOps);
+
+    assertEq(listRecords.getListOpCount(NONCE), 1);
+    _assertBytesEqual(listRecords.getListOp(NONCE, 0), listOps[0]);
+  }
+
+  function test_CanApplyListOpsMultiple() public {
+    assertEq(listRecords.getListOpCount(NONCE), 0);
+
+    bytes[] memory listOps = new bytes[](2);
+    listOps[0] = _encodeListOp(LIST_OP_TYPE_ADD_RECORD);
+    listOps[1] = _encodeListOp(LIST_OP_TYPE_REMOVE_RECORD);
+    listRecords.applyListOps(NONCE, listOps);
+
+    assertEq(listRecords.getListOpCount(NONCE), 2);
+    _assertBytesEqual(listRecords.getListOp(NONCE, 0), listOps[0]);
+    _assertBytesEqual(listRecords.getListOp(NONCE, 1), listOps[1]);
+  }
+
+  function test_RevertIf_applyListOpsWhenPaused() public {
+    listRecords.pause();
+    vm.expectRevert(Error_EnforcedPause);
+    bytes[] memory listOps = new bytes[](2);
+    listOps[0] = _encodeListOp(LIST_OP_TYPE_ADD_RECORD);
+    listOps[1] = _encodeListOp(LIST_OP_TYPE_REMOVE_RECORD);
+    listRecords.applyListOps(NONCE, listOps);
+  }
+}
